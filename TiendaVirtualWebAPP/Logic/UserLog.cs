5bb857856230b85e145b53ff89654c88a179f738
Lina Lopez <linamlopezm05@gmail.com>
2024-10-27 15:07:21 -0500

    Se agregan las funcionalidades CRUD de la clase Usuarios

---
+++ TiendaVirtualWebAPP/Logic/UserLog.cs
@@ -0,0 +1,39 @@
+﻿using Data;
+using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Data;
+using System.Linq;
+using System.Web;
+
+namespace Logic
+{
+    public class UserLog
+    {
+        UserDat objUsers = new UserDat();
+
+        
+        public DataSet showUsers()
+        {
+            return objUsers.showUsers();
+        }
+
+        
+        public bool saveUsers(string _correo, string _contrasena, string _salt, string _estado, string _rol)
+        {
+            return objUsers.saveUsers(_correo, _contrasena, _salt, _estado, _rol);
+        }
+
+        
+        public bool updateUsers(int _idUsers, string _correo, string _contrasena, string _salt, string _estado, string _rol)
+        {
+            return objUsers.updateUsers(_idUsers, _correo, _contrasena, _salt, _estado, _rol);
+        }
+
+        
+        public bool deleteUsers(int _idUsers)
+        {
+            return objUsers.deleteUsers(_idUsers);
+        }
+    }
+}
