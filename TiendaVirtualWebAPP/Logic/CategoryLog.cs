f7075aeb563b2679ba1682ff14aeb97274324297
Lina Lopez <linamlopezm05@gmail.com>
2024-10-27 14:14:30 -0500

    Se agregan las funcionalidades CRUD de la clase Categoria en la capa Logic

---
+++ TiendaVirtualWebAPP/Logic/CategoryLog.cs
@@ -0,0 +1,40 @@
+﻿using Data;
+using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Data;
+using System.Linq;
+using System.Runtime.Remoting;
+using System.Web;
+
+namespace Logic
+{
+    public class CategoryLog
+    {
+        CategoryDat objCategory = new CategoryDat();
+
+
+        public DataSet showCategories()
+        {
+            return objCategory.showCategories();
+        }
+
+      
+        public bool saveCategory(string _nombre)
+        {
+            return objCategory;
+        }
+
+  
+        public bool updateCategory(int _idCategory, string _nombre)
+        {
+            return objCategory;
+        }
+
+
+        public bool deleteCategory(int _idCategory)
+        {
+            return objCategory;
+        }
+    }
+}
