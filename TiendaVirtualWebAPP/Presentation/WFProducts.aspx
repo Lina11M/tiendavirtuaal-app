f89f8979640445b673b2e5cc6463fd39fa718a75
Daniel Adrada <adrada.daniel1018@gmail.com>
2024-11-10 14:37:59 -0500

    Se crea el formulario web forms de la tabla productos

---
+++ TiendaVirtualWebAPP/Presentation/WFProducts.aspx
@@ -0,0 +1,80 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeBehind="WFProducts.aspx.cs" Inherits="Presentation.WFProducts" %>
+
+<!DOCTYPE html>
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+<meta http-equiv="Content-Type" content="text/html; charset=utf-8"/>
+    <title></title>
+</head>
+<body>
+    <form id="form1" runat="server">
+        <div>
+            <%-- ID--%>
+
+            <asp:HiddenField ID="HFProductId" runat="server" />
+
+
+            <%-- Nombre--%>
+            <asp:Label ID="Label1" runat="server" Text="Ingrese el nombre"></asp:Label>
+            <asp:TextBox ID="TBNombre" runat="server"></asp:TextBox>
+            <br />
+            <%--descripcion--%>
+            <asp:Label ID="Label2" runat="server" Text="Ingrese la descripcion"></asp:Label>
+            <asp:TextBox ID="TBDescripcion" runat="server"></asp:TextBox>
+            <br />
+
+             <%--Precio--%>
+            <asp:Label ID="Label3" runat="server" Text="Ingrese el precio"></asp:Label>
+            <asp:TextBox ID="TBPrice" runat="server"></asp:TextBox>
+            <br />
+
+            <%--Stock--%>
+            <asp:Label ID="Label4" runat="server" Text="Ingrese el stock"></asp:Label>
+            <asp:TextBox ID="TBStock" runat="server"></asp:TextBox>
+            <br />
+
+            <%--Imagen--%>
+            <%--<asp:Label ID="Label5" runat="server" Text="Ingrese el stock"></asp:Label>
+            <asp:TextBox ID="TBImagen" runat="server"></asp:TextBox>
+            <br />--%>
+
+            <%--Categoria--%>
+            <asp:Label ID="Label6" runat="server" Text="Seleccione la categoria"></asp:Label>
+            <asp:DropDownList ID="DDLCategoria" runat="server"></asp:DropDownList>
+            <br />
+
+            <%--Proveedores--%>
+            <asp:Label ID="Label7" runat="server" Text="Seleccione el proveedor"></asp:Label>
+            <asp:DropDownList ID="DDLProveedor" runat="server"></asp:DropDownList>
+            <br />
+
+            <%-- Botones--%>
+            <%--<asp:Button ID="BtnSave" runat="server" Text="Guardar" OnClick="BtnSave_Click" />
+            <asp:Button ID="BtnUpdate" runat="server" Text="Actualizar" OnClick="BtnUpdate_Click" />
+            <asp:Label ID="LblMsj" runat="server" Text=""></asp:Label>
+            <br />--%>
+
+            <%--Lidta de productos--%>
+            <%--<asp:GridView ID="GVProducts" runat="server" AutoGenerateColumns="False" OnSelectedIndexChanged="GVProducts_SelectedIndexChanged">
+                <Columns>
+                    <asp:BoundField DataField="pro_id" HeaderText="Pro ID" />
+                    <asp:BoundField DataField="pro_nombre" HeaderText="Nombre" />
+                    <asp:BoundField DataField="pro_descripcion" HeaderText="Descripción" />
+                    <asp:BoundField DataField="pro_precio" HeaderText="Precio" />
+                    <asp:BoundField DataField="pro_stock" HeaderText="Stock" />--%>
+                   <%-- <asp:BoundField DataField="pro_imagen" HeaderText="Imagen" />--%>
+                    <%--<asp:BoundField DataField="tbl_categoria_cat_id" HeaderText="FK Categoría" />
+                    <asp:BoundField DataField="tbl_proveedores_prov_id" HeaderText="FK Proveedor" />
+
+
+                    <asp:CommandField ShowSelectButton="True" />
+                    <asp:CommandField ShowDeleteButton="True" />
+                </Columns>
+            </asp:GridView>--%>
+
+
+        </div>
+    </form>
+</body>
+</html>
---
+++ TiendaVirtualWebAPP/Presentation/WFProducts.aspx.designer.cs
@@ -0,0 +1,143 @@
+﻿//------------------------------------------------------------------------------
+// <generado automáticamente>
+//     Este código fue generado por una herramienta.
+//
+//     Los cambios en este archivo podrían causar un comportamiento incorrecto y se perderán si
+//     se vuelve a generar el código. 
+// </generado automáticamente>
+//------------------------------------------------------------------------------
+
+namespace Presentation
+{
+
+
+    public partial class WFProducts
+    {
+
+        /// <summary>
+        /// Control form1.
+        /// </summary>
+        /// <remarks>
+        /// Campo generado automáticamente.
+        /// Para modificarlo, mueva la declaración del campo del archivo del diseñador al archivo de código subyacente.
+        /// </remarks>
+        protected global::System.Web.UI.HtmlControls.HtmlForm form1;
+
+        /// <summary>
+        /// Control HFProductId.
+        /// </summary>
+        /// <remarks>
+        /// Campo generado automáticamente.
+        /// Para modificarlo, mueva la declaración del campo del archivo del diseñador al archivo de código subyacente.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.HiddenField HFProductId;
+
+        /// <summary>
+        /// Control Label1.
+        /// </summary>
+        /// <remarks>
+        /// Campo generado automáticamente.
+        /// Para modificarlo, mueva la declaración del campo del archivo del diseñador al archivo de código subyacente.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.Label Label1;
+
+        /// <summary>
+        /// Control TBNombre.
+        /// </summary>
+        /// <remarks>
+        /// Campo generado automáticamente.
+        /// Para modificarlo, mueva la declaración del campo del archivo del diseñador al archivo de código subyacente.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.TextBox TBNombre;
+
+        /// <summary>
+        /// Control Label2.
+        /// </summary>
+        /// <remarks>
+        /// Campo generado automáticamente.
+        /// Para modificarlo, mueva la declaración del campo del archivo del diseñador al archivo de código subyacente.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.Label Label2;
+
+        /// <summary>
+        /// Control TBDescripcion.
+        /// </summary>
+        /// <remarks>
+        /// Campo generado automáticamente.
+        /// Para modificarlo, mueva la declaración del campo del archivo del diseñador al archivo de código subyacente.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.TextBox TBDescripcion;
+
+        /// <summary>
+        /// Control Label3.
+        /// </summary>
+        /// <remarks>
+        /// Campo generado automáticamente.
+        /// Para modificarlo, mueva la declaración del campo del archivo del diseñador al archivo de código subyacente.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.Label Label3;
+
+        /// <summary>
+        /// Control TBPrice.
+        /// </summary>
+        /// <remarks>
+        /// Campo generado automáticamente.
+        /// Para modificarlo, mueva la declaración del campo del archivo del diseñador al archivo de código subyacente.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.TextBox TBPrice;
+
+        /// <summary>
+        /// Control Label4.
+        /// </summary>
+        /// <remarks>
+        /// Campo generado automáticamente.
+        /// Para modificarlo, mueva la declaración del campo del archivo del diseñador al archivo de código subyacente.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.Label Label4;
+
+        /// <summary>
+        /// Control TBStock.
+        /// </summary>
+        /// <remarks>
+        /// Campo generado automáticamente.
+        /// Para modificarlo, mueva la declaración del campo del archivo del diseñador al archivo de código subyacente.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.TextBox TBStock;
+
+        /// <summary>
+        /// Control Label6.
+        /// </summary>
+        /// <remarks>
+        /// Campo generado automáticamente.
+        /// Para modificarlo, mueva la declaración del campo del archivo del diseñador al archivo de código subyacente.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.Label Label6;
+
+        /// <summary>
+        /// Control DDLCategoria.
+        /// </summary>
+        /// <remarks>
+        /// Campo generado automáticamente.
+        /// Para modificarlo, mueva la declaración del campo del archivo del diseñador al archivo de código subyacente.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.DropDownList DDLCategoria;
+
+        /// <summary>
+        /// Control Label7.
+        /// </summary>
+        /// <remarks>
+        /// Campo generado automáticamente.
+        /// Para modificarlo, mueva la declaración del campo del archivo del diseñador al archivo de código subyacente.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.Label Label7;
+
+        /// <summary>
+        /// Control DDLProveedor.
+        /// </summary>
+        /// <remarks>
+        /// Campo generado automáticamente.
+        /// Para modificarlo, mueva la declaración del campo del archivo del diseñador al archivo de código subyacente.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.DropDownList DDLProveedor;
+    }
+}
