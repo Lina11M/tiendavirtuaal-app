3ceafe42e18e602e3c0dc7099e46bf88f7516d29
Lina Lopez <linamlopezm05@gmail.com>
2024-11-10 19:50:00 -0500

    Se crea las funcionalidades de crear, mostrar y actualizar un comentario

---
+++ TiendaVirtualWebAPP/Presentation/WFComments.aspx
@@ -0,0 +1,47 @@
+﻿<%@ Page Title="" Language="C#" MasterPageFile="~/Main.Master" AutoEventWireup="true" CodeBehind="WFComments.aspx.cs" Inherits="Presentation.WebForm1" %>
+<asp:Content ID="Content1" ContentPlaceHolderID="head" runat="server">
+</asp:Content>
+<asp:Content ID="Content2" ContentPlaceHolderID="ContentPlaceHolder1" runat="server">
+    <h1>Gestion de Comentarios</h1>
+    <div>
+        <%--Id--%>
+        <asp:HiddenField ID="HFCommentId" runat="server" />  <%--Permite coultar la llave primearia--%>
+        <br />
+        <%--Comentario--%>
+        <asp:Label ID="Label1" runat="server" Text="Escriba un comentario"></asp:Label>
+        <asp:TextBox ID="TBComment" runat="server"></asp:TextBox>
+        <br />
+        <%--Fecha Comentario--%>
+        <asp:Label ID="Label2" runat="server" Text="Fecha del comentario"></asp:Label>
+        <asp:TextBox ID="TBFecha" runat="server"></asp:TextBox>
+        <br />
+        <%--Producto--%>
+        <asp:Label ID="Label6" runat="server" Text="Seleccione el producto"></asp:Label>
+        <asp:DropDownList ID="DDLProduct" runat="server"></asp:DropDownList>
+        <br />
+        <%--Cliente--%>
+        <asp:Label ID="Label7" runat="server" Text="Seleccione el cliente"></asp:Label>
+        <asp:DropDownList ID="DDLClient" runat="server" OnSelectedIndexChanged="DDLClient_SelectedIndexChanged"></asp:DropDownList>
+        <br />
+
+        <asp:Button ID="BtnSave" runat="server" Text="Guardar" OnClick="BtnSave_Click" />
+        <asp:Button ID="BtnUpdate" runat="server" Text="Actualizar" OnClick="BtnUpdate_Click" />
+        <asp:Label ID="LblMsj" runat="server" Text=""></asp:Label>
+        <br />
+
+        <%--Lista de comentarios--%>
+        <asp:GridView ID="GVComment" runat="server" AutoGenerateColumns="False" OnSelectedIndexChanged="GVComment_SelectedIndexChanged">
+            <%--Se agrega la propiedad "Columns" que permite personalizar los nombres de las columnas--%>
+            <Columns>
+                <asp:BoundField DataField="com_id" HeaderText="Id" />
+                <asp:BoundField DataField="com_comentario" HeaderText="Codigo" />
+                <asp:BoundField DataField="com_fecha" HeaderText="Fecha" />
+                <asp:BoundField DataField="tbl_productos_pro_id" HeaderText="Producto" />
+                <asp:BoundField DataField="tbl_clientes_cli_id" HeaderText="Cliente" />
+                <asp:CommandField ShowSelectButton="True"></asp:CommandField>
+                <asp:CommandField ShowDeleteButton="True"></asp:CommandField>
+            </Columns>
+        </asp:GridView>
+
+    </div>
+</asp:Content>
---
+++ TiendaVirtualWebAPP/Presentation/WFComments.aspx.designer.cs
@@ -0,0 +1,134 @@
+﻿//------------------------------------------------------------------------------
+// <generado automáticamente>
+//     Este código fue generado por una herramienta.
+//
+//     Los cambios en este archivo podrían causar un comportamiento incorrecto y se perderán si
+//     se vuelve a generar el código. 
+// </generado automáticamente>
+//------------------------------------------------------------------------------
+
+namespace Presentation
+{
+
+
+    public partial class WebForm1
+    {
+
+        /// <summary>
+        /// Control HFCommentId.
+        /// </summary>
+        /// <remarks>
+        /// Campo generado automáticamente.
+        /// Para modificarlo, mueva la declaración del campo del archivo del diseñador al archivo de código subyacente.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.HiddenField HFCommentId;
+
+        /// <summary>
+        /// Control Label1.
+        /// </summary>
+        /// <remarks>
+        /// Campo generado automáticamente.
+        /// Para modificarlo, mueva la declaración del campo del archivo del diseñador al archivo de código subyacente.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.Label Label1;
+
+        /// <summary>
+        /// Control TBComment.
+        /// </summary>
+        /// <remarks>
+        /// Campo generado automáticamente.
+        /// Para modificarlo, mueva la declaración del campo del archivo del diseñador al archivo de código subyacente.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.TextBox TBComment;
+
+        /// <summary>
+        /// Control Label2.
+        /// </summary>
+        /// <remarks>
+        /// Campo generado automáticamente.
+        /// Para modificarlo, mueva la declaración del campo del archivo del diseñador al archivo de código subyacente.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.Label Label2;
+
+        /// <summary>
+        /// Control TBFecha.
+        /// </summary>
+        /// <remarks>
+        /// Campo generado automáticamente.
+        /// Para modificarlo, mueva la declaración del campo del archivo del diseñador al archivo de código subyacente.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.TextBox TBFecha;
+
+        /// <summary>
+        /// Control Label6.
+        /// </summary>
+        /// <remarks>
+        /// Campo generado automáticamente.
+        /// Para modificarlo, mueva la declaración del campo del archivo del diseñador al archivo de código subyacente.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.Label Label6;
+
+        /// <summary>
+        /// Control DDLProduct.
+        /// </summary>
+        /// <remarks>
+        /// Campo generado automáticamente.
+        /// Para modificarlo, mueva la declaración del campo del archivo del diseñador al archivo de código subyacente.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.DropDownList DDLProduct;
+
+        /// <summary>
+        /// Control Label7.
+        /// </summary>
+        /// <remarks>
+        /// Campo generado automáticamente.
+        /// Para modificarlo, mueva la declaración del campo del archivo del diseñador al archivo de código subyacente.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.Label Label7;
+
+        /// <summary>
+        /// Control DDLClient.
+        /// </summary>
+        /// <remarks>
+        /// Campo generado automáticamente.
+        /// Para modificarlo, mueva la declaración del campo del archivo del diseñador al archivo de código subyacente.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.DropDownList DDLClient;
+
+        /// <summary>
+        /// Control BtnSave.
+        /// </summary>
+        /// <remarks>
+        /// Campo generado automáticamente.
+        /// Para modificarlo, mueva la declaración del campo del archivo del diseñador al archivo de código subyacente.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.Button BtnSave;
+
+        /// <summary>
+        /// Control BtnUpdate.
+        /// </summary>
+        /// <remarks>
+        /// Campo generado automáticamente.
+        /// Para modificarlo, mueva la declaración del campo del archivo del diseñador al archivo de código subyacente.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.Button BtnUpdate;
+
+        /// <summary>
+        /// Control LblMsj.
+        /// </summary>
+        /// <remarks>
+        /// Campo generado automáticamente.
+        /// Para modificarlo, mueva la declaración del campo del archivo del diseñador al archivo de código subyacente.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.Label LblMsj;
+
+        /// <summary>
+        /// Control GVComment.
+        /// </summary>
+        /// <remarks>
+        /// Campo generado automáticamente.
+        /// Para modificarlo, mueva la declaración del campo del archivo del diseñador al archivo de código subyacente.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.GridView GVComment;
+    }
+}
